146d1e6687fbede1543fe05cba5bdf65fd0f00d6
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2023-02-01 21:00:38 +0100

    basic version

--- bib-picker/src/main.rs
+++ bib-picker/src/main.rs
@@ -1,31 +1,65 @@
 use biblatex::Bibliography;
-use std::collections::HashMap;
-use biblatex::Chunk;
 use itertools::Itertools;
-use std::env;
+use skim::prelude::*;
+use std::collections::HashMap;
 use std::fs;
-
+use std::io::Cursor;
+use std::env;
 
 fn main() {
-    let file_path = "refs.bib";
     
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
-    // TODO: minuscule for authors, title    
-    let bibliography = Bibliography::parse(&contents).unwrap();
-    bibliography.iter().for_each(|entry| {
-        let author_field: String = entry.author().unwrap().iter().map(|p| format!("{} {}", p.name, p.given_name)).intersperse(", ".to_string()).collect();
-        let title_field: String = entry.title().unwrap().iter().map(|spanned| spanned.as_ref()).map(|x| format!("{}", x.v.get())).intersperse(String::from("")).collect();
-        println!("{}: {} ({})", entry.key, title_field, author_field);        
-    });
-    let values: Vec<_> = bibliography.iter().map(|entry| {
-        let author_field: String = entry.author().unwrap().iter().map(|p| format!("{} {}", p.name, p.given_name)).intersperse(", ".to_string()).collect();
-        let title_field: String = entry.title().unwrap().iter().map(|spanned| spanned.as_ref()).map(|x| format!("{}", x.v.get())).intersperse(String::from("")).collect();
-        (entry.key.clone(), format!("{} ({})", title_field, author_field))
-    }).collect();
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
 
-    println!("{:#?}", values);
-    let myhm: HashMap<String, String> = HashMap::from_iter(values.into_iter());
-    println!("{:#?}", myhm);
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let clean_contents = contents.replace("AUTHOR", "author").replace("TITLE", "title");
+    // TODO: minuscule for authors, title
+    let bibliography = Bibliography::parse(&clean_contents).unwrap();
+    let values: Vec<_> = bibliography
+        .iter()
+        .filter(|e| e.author().is_ok())
+        .map(|entry| {
+            let author_field: String = entry
+                .author()
+                .unwrap()
+                .iter()
+                .map(|p| format!("{} {}", p.name, p.given_name))
+                .intersperse(", ".to_string())
+                .collect();
+            let title_field: String = entry
+                .title()
+                .unwrap()
+                .iter()
+                .map(|spanned| spanned.as_ref())
+                .map(|x| format!("{}", x.v.get()))
+                .intersperse(String::from(""))
+                .collect();
+            (
+                format!("{} ({})", title_field, author_field),
+                entry.key.clone(),
+            )
+        })
+        .collect();
 
+    let myhm: HashMap<String, String> = HashMap::from_iter(values.clone().into_iter());
+    let options = SkimOptionsBuilder::default()
+        .height(Some("50%"))
+        .multi(true)
+        .build()
+        .unwrap();
+
+    let input = values.iter().map(|(k, _v)| k.clone()).join("\n");
+
+    let item_reader = SkimItemReader::default();
+    let items = item_reader.of_bufread(Cursor::new(input));
+
+    let selected_items = Skim::run_with(&options, Some(items))
+        .map(|out| out.selected_items)
+        .unwrap_or_else(|| Vec::new());
+
+    for item in selected_items.iter() {
+        let key: String = item.output().to_string();
+        let value = myhm.get(&key).unwrap();
+        println!("{}", value);
+    }
 }
